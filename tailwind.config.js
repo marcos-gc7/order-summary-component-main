305dad4357a8ac5592b91c971012234134fba27f
Marcos Castro <marcos.yk07@gmail.com>
2025-09-15 17:09:12 -0300

    feat: complete mobile layout created

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // <- importante!
+  theme: {
+    extend: {
+      fontFamily: {
+        redhat: ['"Red Hat Display"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
